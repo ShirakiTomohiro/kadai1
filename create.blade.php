6ee4ca334f46cf40f90cb23b16b87bd5814fddf6
ShirakiTomohiro <50699450+ShirakiTomohiro@users.noreply.github.com>
2019-05-26 14:43:14 +0900

    Create create.blade.php

---
+++ create.blade.php
@@ -0,0 +1,16 @@
+# kadai1
+<!DOCTYPE html>
+<html>
+  <head>
+     <meta charset="UTF-8">
+     <meta https-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="vieport" content="width=device-width,initial-scale=1"
+>
+
+     <title>MyNews</title>
+  </head>
+  <body>
+     <h1>Myニュース作成画面</h1>
+     <p>原宿で買い物をした</p>
+  </body>
+</html>
