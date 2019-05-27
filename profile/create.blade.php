ec11aa3a889a1285a44bbb2840273d5fc6f89f28
ShirakiTomohiro <50699450+ShirakiTomohiro@users.noreply.github.com>
2019-05-27 16:02:07 +0900

    PHP/Laravel 12の課題

---
+++ profile/create.blade.php
@@ -0,0 +1,17 @@
+# kadai1
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+{{-- profile.blade.phpの@yieldに('title')'ニュースの新規作成'を埋め込む --}}
+@section('title','ニュースの新規作成')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+  <div class="container">
+     <div class="row">
+         <div class="col-md-8 mx-auto">
+            <h2>ニュース新規作成</h2>
+         </div>
+     </div>
+  </div>
+@endsection
