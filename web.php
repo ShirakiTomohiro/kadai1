b1c4f665d51d2b512656b0913a2811ebb4551ffa
ShirakiTomohiro <50699450+ShirakiTomohiro@users.noreply.github.com>
2019-06-01 14:10:01 +0900

    Add files via upload

--- web.php
+++ web.php
@@ -28,5 +28,12 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+ });
+
+ Route::group(['prefix' =>'admin', 'middleware'=> 'auth'], function() {
+      Route::get('news/create', 'Admin\NewsController@add');
+      Route::post('news/create', 'Admin\NewsController@create'); # 追記
+      Route::post('profile/create', 'Admin\ProfileController@create');
+      Route::post('profile/edit', 'Admin\ProfileController@update');
  });
