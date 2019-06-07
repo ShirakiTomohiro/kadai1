633b042c10bde252e26538754aa6d55d428b9427
ShirakiTomohiro <50699450+ShirakiTomohiro@users.noreply.github.com>
2019-06-07 17:47:24 +0900

    Add files via upload

--- edit.blade.php
+++ edit.blade.php
@@ -1,17 +1,49 @@
-# kadai1
-<!DOCTYPE html>
-<html>
-  <head>
-     <meta charset="UTF-8">
-     <meta https-equiv="X-UA-Compatible" content="IE=edge">
-     <meta name="vieport" content="width=device-width,initial-scale=1"
->
+@extends('layouts.profile')
+@section('title', 'ニュースの編集')
 
-     <title>MyNews</title>
-  </head>
-  <body>
-     <h1>Myニュース作成画面</h1>
-     <p1>渋谷に行った</p>
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>profile</h2>
+                <form action="{{ action('Admin\ProfileController@update') }}"
+method="post" enctype="multipart/form-data">
+                   @if (count($errors) > 0)
+                <ul>
 
-  </body>
-</html>
+                   @endif
+                   <div class="form-group row">
+                       <label class="col-md-2" for="title">name</label>
+                       <div class="col-md-10">
+                           <input type="text" class="form-control"
+                    name="name" value="{{ $profile_form->name}}">
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                      <label class="col-md-2" for="body">gender</label>
+                      <div class="col-md-10">
+                          <input type="text" class="form-control"
+                    name="gender" value="{{ $profile_form->gender}}">
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                      <label class="col-md-2" for="title">hobby</label>
+                      <div class="col-md-10">
+                          <input type="text" class="form-control"
+                          name="hobby" value="{{ $profile_form->hobby}}">
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                    <label class="col-md-2" for="body">introduction</label>
+                    <div class="col-md-10">
+                      <textarea class="form-control" name="introduction"
+                    rows="20">{{ $profile_form->introduction }}</textarea>
+                    </div>
+                  </div>
+                  {{ csrf_field() }}
+                  <input type="submit" class="btn btn-primary" value="更新">
+              </form>
+          </div>
+      </div>
+  </div>
+@endsection
