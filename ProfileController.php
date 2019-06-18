c226c9ce1cce45c524a5df71f02d2fa79598bf04
ShirakiTomohiro <50699450+ShirakiTomohiro@users.noreply.github.com>
2019-06-18 11:16:42 +0900

    課題17

--- ProfileController.php
+++ ProfileController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-use App\Profile;
+use App\Profiles;
+use App\Historie;
+use Carbon\Carbon;
 
 class ProfileController extends Controller
 {
@@ -17,54 +19,79 @@ class ProfileController extends Controller
   public function create(Request $request)
   {
     // Validationを行う
-    $this->validate($request, Profile::$rules);
+    $this->validate($request, Profiles::$rules);
 
-    $profile = new Profile;
+    $profiles = new Profiles;
     $form = $request->all();
 
 
-
+   // フォームから送信されてきた_tokenを削除する
     unset($form['_token']);
 
 
     //データベースに保存する
-    $profile->fill($form);
-    $profile->save();
+    $profiles->fill($form);
+    $profiles->save();
 
     return redirect('admin/profile/create');
+  }
+
+  public function index(Request $request)
+  {
+     $cond_title = $request->cond_title;
+     if ($cond_title != '') {
+         //検索されたら検索結果を取得する
+         $posts = Profiles::where('title', $cond_title)->get();
+    } else {
+        // それ以外はすべてのニュースを取得する
+        $posts = Profiles::all();
+    }
+    return view ('admin.profile.index', ['posts' => $posts, 'cond_title' =>$cond_title]);
   }
 
   public function edit(Request $request)
   {
       // Profile Modelからデータを所得する
-      $profile = Profile::find($request->id);
-      if (empty($profile)) {
-        // abort(404);
+      $profiles = Profiles::find($request->id);
+print_r($profiles);
+      if (empty($profiles)) {
+        //abort(404);
       }
-      return view('admin.profile.edit', ['profile_form' => $profile]);
+      return view('admin.profile.edit', ['profiles_form' => $profiles]);
   }
 
   public function update(Request $request)
   {
     // Validationをかける
-    $this->validate($request,Profile::$rules);
+    $this->validate($request,Profiles::$rules);
     // Profile Modelからデータを取得する
-    $profile = Profile::find($request->id);
+    $profiles = Profiles::find($request->id);
     // 送信されてきたフォームデータを格納する
-    $profile_form = $request->all();
-      if(isset($profile_form['image'])) {
-        $path = $request->file('image')->store('public/image');
-        $profile->image_path = basename($path);
+    $profiles_form = $request->all();
 
-      } else {
-          $profile->image_path = null;
-      }
-     unset($profile_form['_token']);
-     unset($profile_form['remove']);
+     unset($profiles_form['_token']);
+     unset($profiles_form['remove']);
+
     // 該当するデータを上書きして保存する
-      $profile->fill($form);
-      $profile->save();
+      $profiles->fill($profiles_form)->save();
 
-    return redirect('admin/profile');
+      //以下を追記
+      $histories = new Historie;
+      $histories->profiles_id = $profiles->id;
+      $histories->edited_at = Carbon::now();
+      $histories->save();
+
+    return redirect('admin/profile/edit?id='.$request->id);
   }
+
+  // 以下を追記
+  public function delete(Request $request)
+  {
+    // 該当するProfiles Modelを取得
+    $profiles = Profiles::find($request->id);
+    // 削除する
+    $profiles->delete();
+    return redirect('admin/profile/edit?id='.$request->id);
+  }
+
 }
